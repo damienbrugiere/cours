b4e4c5521d5796d27550341289b5a5dc76a29b1b
BRUGIERE DAMIEN <damien.brugiere.btssio@gmail.com>
2019-05-18 16:15:22 +0200

    solution exo 1

--- src/main/java/cours/Main.java
+++ src/main/java/cours/Main.java
@@ -1,3 +1,7 @@
+package cours;
+
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -7,8 +11,57 @@
  *
  */
 public class Main {
-	
+
 	public static void main(String[] args) {
-		System.out.println("yo");
+		char[][] terrain = new char[4][4];
+		int persoI = 0;
+		int persoJ = 0;
+		String in = null;
+		Scanner sc = new Scanner(System.in);
+		do {
+			if (in != null) {
+				switch (in) {
+				case "z":
+					if(persoI!=0) {
+						persoI--;
+					}
+					break;
+				case "q":
+					if(persoJ!=0) {
+						persoJ--;
+					}
+					break;
+				case "d":
+					if(persoJ!=4) {
+						persoJ++;
+					}
+					break;
+				case "s":
+					if(persoI!=4) {
+						persoI++;
+					}
+					break;
+				default:
+					break;
+				}
+			}
+			display(terrain, persoI, persoJ);
+			in = sc.nextLine();
+		} while (!in.equals("exit"));
+
 	}
+
+	private static void display(char[][] terrain, int persoI, int persoJ) {
+		for (int i = 0; i <= terrain.length; i++) {
+			for (int j = 0; j <= terrain[0].length; j++) {
+				if (i == persoI && j == persoJ) {
+					System.out.print("o");
+				} else {
+					System.out.print(".");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
 }
